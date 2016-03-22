37d66b24966acefc1ea20bd1eff4b08ae356498f
sridevij <sridevi.160@gmail.com>
2016-03-22 10:50:19 -0500

    step1

--- module4-solution/js/script.js
+++ module4-solution/js/script.js
@@ -83,7 +83,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl, 
-  function (responseText) {
+ function (responseText) {
     document.querySelector("#main-content")
       .innerHTML = responseText;
   }, // ***** <---- TODO: STEP 1: Substitute [...] ******
