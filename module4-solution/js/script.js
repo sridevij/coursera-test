9b7ba0c2d26570b20bf26312c9d6a31cc928e9a0
sridevij <sridevi.160@gmail.com>
2016-03-22 10:14:01 -0500

    step1

--- module4-solution/js/script.js
+++ module4-solution/js/script.js
@@ -83,7 +83,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl, 
-  [...], // ***** <---- TODO: STEP 1: Substitute [...] ******
+  function (responseText) {
+    document.querySelector("#main-content")
+      .innerHTML = responseText;
+  }, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
