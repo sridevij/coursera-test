4c9415ca7ee11fe813acb2d94f4b23981db5dce0
sridevij <sridevi.160@gmail.com>
2016-03-23 10:29:58 -0500

    test

--- module4-solution/js/script.js
+++ module4-solution/js/script.js
@@ -151,7 +151,7 @@ html=insertProperty(homeHtmlUrl, "randomCategoryShortName", chosenCategoryShortN
 function chooseRandomCategory (categories) {
   // Choose a random index into the array (from 0 inclusively until array length (exclusively))
   var randomArrayIndex = Math.floor(Math.random() * categories.length);
-
+  console.log(rangonArrayIndex);
   // return category object with that randomArrayIndex
   return categories[randomArrayIndex];
 }
