f4d495c6ee28884a0e1b80560ec34bd2aa9e6596
sridevij <sridevi.160@gmail.com>
2016-03-23 17:18:31 -0500

    test

--- module4-solution/js/script.js
+++ module4-solution/js/script.js
@@ -169,12 +169,11 @@ dc.loadMenuCategories = function () {
 // Load the menu items view
 // 'categoryShort' is a short_name for a category
 dc.loadMenuItems = function (categoryShort) {
-   console.log("loadMenuItems arg="+categoryShort);
-  var menu=menuItemsUrl+categoryShort;
+   console.log("loadMenuItems arg="+categoryShort)
   showLoading("#main-content");
   $ajaxUtils.sendGetRequest(
     menuItemsUrl + categoryShort,
-    buildAndShowMenuItemsHTML(menu));
+    buildAndShowMenuItemsHTML;
 };
 
 
